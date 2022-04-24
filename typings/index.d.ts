aff22b86ac60dffda47e675ed2f588c55e5a26c9
xhyu <xhyu@qeelyn.com>
2022-04-24 18:24:16 +0800

    暂存定义

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,9 +1,20 @@
-import { RuleType, PropsOptionType, ApiFnType } from '../components/types'
+import type { Component, App } from 'vue'
+import type { RuleType, PropsOptionType, ApiFnType } from '../components/types'
 
 declare module 'json-layout' {
-    export const rule: RuleType;
-    export const api: ApiFnType;
-    export const option: PropsOptionType;
-    export const modelValue: any
-    export const isForm: boolean
+    export const name = 'JsonLayout';
+
+    export const components: Record<string, Component>;
+    export interface props {
+        rule: RuleType;
+        api: ApiFnType;
+        option: PropsOptionType;
+        modelValue: any
+        isForm: boolean
+        "update:api": Function
+        "update:modelValue": Function
+    }
+
+    export function install(app: App): App
+
 }
