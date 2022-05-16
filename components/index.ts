b2efc3d537d40d0db7700bae66b27a209587cee3
xhyu <xhyu@qeelyn.com>
2022-05-16 09:23:37 +0800

    打包调整

--- components/index.ts
+++ components/index.ts
@@ -1,12 +1,10 @@
 import type { App } from 'vue';
 import type { FactoryOptionType } from './types';
 import factory from './core';
-import pkg from '../package.json';
 
 export const JsonLayout = factory();
 
 const useComponent = {
-  version: pkg.version,
   setOption: (option: FactoryOptionType) => {
     JsonLayout.setOption(option)
   },
