229823d47842897bd11639d3f682f115a04090a4
xhyu <xhyu@qeelyn.com>
2022-07-07 15:50:57 +0800

    调整文件

--- webpack.config.js
+++ webpack.config.js
@@ -13,8 +13,6 @@ module.exports = {
     output: {
         filename: '[name].js',
         path: path.resolve(__dirname, 'dist'),
-        // library: "BeastForm",
-        // libraryTarget: "umd"
     },
     module: {
         rules: [
